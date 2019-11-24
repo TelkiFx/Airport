968aa29fe93e925d62cb10389f6eae89ff9a6bd3
Cameron Tredoux <camtredoux@gmail.com>
2019-11-23 17:05:29 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,6 @@
 public class Main {
 
   public static void main(String[] args) {
-//    Testing cases - need to alter Scanner in AirportPanel.java
-//    String input =
-//        "1\n" + "HA-LOL\n" + "42\n" +
-//        "1\n" + "G-OWAC\n" + "101\n" +
-//        "2\n" + "HA-LOL\n" + "HEL\n" + "BAL\n" +
-//        "2\n" + "G-OWAC\n" + "JFK\n" + "BAL\n" +
-//        "2\n" + "HA-LOL\n" + "BAL\n" + "HEL\n" +
-//        "x\n" +
-//        "1\n" + "2\n" +
-//        "3\n" + "G-OWAC\n" +
-//        "3\n" + "HA-LOL\n" +
-//        "x\n";
-//    Scanner test = new Scanner(input);
-//    AirportPanel airportPanel = new AirportPanel(test);
-
     AirportPanel airportPanel = new AirportPanel();
 
     airportPanel.start();
